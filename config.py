fd361acaf41d2bb20ca35ea5eebc4ecae300b4fa
kokokuo <v6610688@gmail.coom>
2019-05-23 15:36:20 +0800

    修改為 Python 3 ，使用 Pipenv 取代 requirements.txt

--- config.py
+++ config.py
@@ -2,22 +2,22 @@
 # -*- coding: utf-8 -*-
 from collections import namedtuple
 
-ROOT_URL = 'http://taiwanstay.net.tw'
+ROOT_URL = "https://taiwanstay.net.tw"
 
-County = namedtuple('County', ['code', 'text'])
+County = namedtuple("County", ["code", "text"])
 
 # 城市代碼，可以重此切換
-CITY_CODE = 'R'
+CITY_CODE = "R"
 
 # 網頁的查詢參數
 PAYLOAD = {
-	'hotelCity': None, 
-	'hotelCounty': None,
-	'host': False,
-	'conference': False, 
-	'accessible':False,
-	'accessibility': False,
-	'roompricelow':False,
-	'page':1,
-	'listPage':10
+    "hotelCity": None,
+    "hotelCounty": None,
+    "host": False,
+    "conference": False,
+    "accessible": False,
+    "accessibility": False,
+    "roompricelow": False,
+    "page": 1,
+    "listPage": 10
 }
