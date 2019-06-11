df442b5aa9ae24864610068357da6b371ca422ab
kokokuo <v6610688@gmail.coom>
2019-06-11 11:02:19 +0800

    重構呼叫 TaiwanHotelParserAgent 方法，ExcelStore 改由 parsing 時傳入

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
 # -*- coding: utf-8 -*-
 import xlsxwriter
 from parser.agent import TaiwanHotelParserAgent
+from parser.settings import TaiwanHotelConfig
 from store.excel import ExcelStore
 
 
 def main():
-    taiwan_cities_code = {
-        "F": "新北市", "A": "臺北市", "H": "桃園市", "B": "臺中市", "R": "臺南市",
-        "S": "高雄市", "G": "宜蘭縣", "J": "新竹縣", "K": "苗栗縣", "N": "彰化縣",
-        "M": "南投縣", "P": "雲林縣", "Q": "嘉義縣", "T": "屏東縣", "V": "臺東縣",
-        "U": "花蓮縣", "X": "澎湖縣", "C": "基隆市", "O": "新竹市", "I": "嘉義市",
-        "W": "金門縣", "Z": "連江縣"
-    }
-
     print("寫選擇要抓取的資料: ")
-    cities_code = TaiwanHotelParserAgent.CITIES_CODE
+    cities_code = TaiwanHotelConfig.CITIES_CODE
     for city_code in cities_code.keys():
         print("縣市: {}, 輸入的代碼: {}".format(cities_code[city_code], city_code))
     city_code = input("請輸入代碼 (大寫) >> ").upper()
@@ -23,11 +16,11 @@ def main():
         exit()
 
     # 保存要抓取的縣市 xlsx 名稱
-    filename = taiwan_cities_code[city_code] + "所有旅宿統計資料.xlsx"
+    filename = cities_code[city_code] + "所有旅宿統計資料.xlsx"
     excelstore = ExcelStore(filename)
     # workbook = xlsxwriter.Workbook(taiwan_cities_code[city_code] + "所有旅宿統計資料.xlsx")
-    hotel_parser = TaiwanHotelParserAgent(city_code, excelstore)
-    hotel_parser.start_parsing()
+    hotel_parser = TaiwanHotelParserAgent(city_code)
+    hotel_parser.parsing(excelstore)
 
 
 if __name__ == "__main__":
